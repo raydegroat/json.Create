bee9deedfe1bb302ea170ae7dd4c609e0f96b582
Ray de Groat <stockholmray@gmail.com>
2020-09-08 14:58:21 +0200

    Add project files.

---
+++ json.Create/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonCreate
+{
+    class Employee
+    {
+        public string FirstName = "Sam";
+        public string LastName = "Jackson";
+        public int employeeID = 5698532;
+        public string Designation = "Manager";
+    }
+}
---
+++ json.Create/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string JSONresult = JsonConvert.SerializeObject(emp);
+
+            string path = @"C:\json\employee.json";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
